5b2c168952df5f792c3bd1a66b075de8d1239fad
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-06-25 11:41:58 -0400

    bug 454676 - mozillabuild should export APR_ICONV_PATH

--- profile-extrapaths.sh
+++ profile-extrapaths.sh
@@ -5,5 +5,6 @@ if test -n "$MOZILLABUILD"; then
     PATH="/local/bin:$MSYS_MOZBUILD/wget:$MSYS_MOZBUILD/7zip:$MSYS_MOZBUILD/blat261/full:$MSYS_MOZBUILD/python25:$MSYS_MOZBUILD/svn-win32-1.6.3/bin:$MSYS_MOZBUILD/upx203w:$MSYS_MOZBUILD/xemacs/XEmacs-21.4.19/i586-pc-win32:$MSYS_MOZBUILD/info-zip:$MSYS_MOZBUILD/nsis-2.22:$MSYS_MOZBUILD/nsis-2.33u:$MSYS_MOZBUILD/hg:$MSYS_MOZBUILD/python25/Scripts:$MSYS_MOZBUILD/kdiff3:$PATH"
     EDITOR=xemacs.exe
     CVS_RSH=ssh
-    export PATH EDITOR CVS_RSH
+    APR_ICONV_PATH="$MSYS_MOZBUILD/svn-win32-1.6.3/iconv"
+    export PATH EDITOR CVS_RSH APR_ICONV_PATH
 fi
