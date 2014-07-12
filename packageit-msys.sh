fbcb73465246f2e7df4f3a022682f056a6a192bf
Ryan VanderMeulen <ryanvm@gmail.com>
2014-07-11 23:05:26 -0400

    Bug 791752 - Update tar to version 1.22.

--- packageit-msys.sh
+++ packageit-msys.sh
@@ -57,6 +57,7 @@ tar -jvxf "${MSYS_SRCDIR}/mktemp-1.6-1-msys-1.0.11-bin.tar.bz2" -C "${MSYS_STAGE
 tar -jvxf "${MSYS_SRCDIR}/openssh-4.6p1-MSYS-1.0.11.tar.bz2" -C "${MSYS_STAGEDIR}/mozilla-build/msys"
 tar -jvxf "${MSYS_SRCDIR}/openssl-0.9.8e-3-MSYS-1.0.11.tar.bz2" -C "${MSYS_STAGEDIR}/mozilla-build/msys"
 tar -jvxf "${MSYS_SRCDIR}/sed-4.2.1-1-msys-1.0.11-bin.tar.bz2" -C "${MSYS_STAGEDIR}/mozilla-build/msys"
+tar -jvxf "${MSYS_SRCDIR}/tar-1.22-1-msys-1.0.11-bin.tar.bz2" -C "${MSYS_STAGEDIR}/mozilla-build/msys"
 tar -jvxf "${MSYS_SRCDIR}/zlib-1.2.3-MSYS-1.0.11.tar.bz2" -C "${MSYS_STAGEDIR}/mozilla-build/msys"
 # Replace the native MSYS rm with winrm and move the tarballs that extract to usr/ up a level.
 cp "${MSYS_SRCDIR}/winrm.exe" "${MSYS_STAGEDIR}/mozilla-build/msys/bin"
