cc27242c7fe4ff17775e4508c40f9ddccf5d9923
Ryan VanderMeulen <ryanvm@gmail.com>
2014-07-15 09:24:58 -0400

    Bug 1038504 - Remove Time-HiRes and MIME-Base64 from MozillaBuild.

--- packageit-msys.sh
+++ packageit-msys.sh
@@ -13,25 +13,8 @@ fi
 MSYS_SRCDIR=$(cd "$MOZ_SRCDIR" && pwd)
 MSYS_STAGEDIR=$(cd "$MOZ_STAGEDIR" && pwd)
 
-rm -rf "${MSYS_STAGEDIR}"{MIME_Base64-3.07,Time-HiRes-1.9707,make-3.81.90,cvs-1.11.22}
-
-tar -xzf "${MSYS_SRCDIR}/MIME-Base64-3.07.tar.gz" -C "${MSYS_STAGEDIR}"
-pushd "${MSYS_STAGEDIR}/MIME-Base64-3.07"
-perl Makefile.pl
-make LD="gcc -shared"
-make test
-make install PREFIX="${MSYS_STAGEDIR}/mozilla-build/msys"
-popd
-
-tar -xzf "${MSYS_SRCDIR}/Time-HiRes-1.9707.tar.gz" -C "${MSYS_STAGEDIR}"
-pushd "${MSYS_STAGEDIR}/Time-HiRes-1.9707"
-perl Makefile.pl
-make LD="gcc -shared"
-# make test fails because of misconfigured virtual internal timers :-(
-make install PREFIX="${MSYS_STAGEDIR}/mozilla-build/msys"
-popd
-
 # The make 3.81 shipping with msys is still broken, so continue shipping this.
+rm -rf "${MSYS_STAGEDIR}/make-3.81.90"
 tar -xjf "${MSYS_SRCDIR}/make-3.81.90.tar.bz2" -C "${MSYS_STAGEDIR}"
 pushd "${MSYS_STAGEDIR}/make-3.81.90"
 patch -p0 < "${MSYS_SRCDIR}/make-msys.patch"
