e8db1f17023d29ca11998d8790ab2993caac859e
Ryan VanderMeulen <ryanvm@gmail.com>
2015-02-18 12:23:33 -0500

    Bug 1128186 - Move all non-error output to profile-echo.sh. r=ted

--- start-shell.bat
+++ start-shell.bat
@@ -3,16 +3,16 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 REM Reset some env vars and set some others.
+SET CYGWIN=
 SET INCLUDE=
 SET LIB=
-SET CYGWIN=
 IF NOT DEFINED MOZ_NO_RESET_PATH (
   SET PATH=%SystemRoot%\System32;%SystemRoot%;%SystemRoot%\System32\Wbem
 )
+SET ERROR=
+SET TOOLCHAIN=
 
 SET MOZILLABUILD=%~dp0
-
-ECHO MozillaBuild Install Directory: %MOZILLABUILD%
 
 REM Figure out if we're on a 32-bit or 64-bit host OS.
 REM NOTE: Use IF ERRORLEVEL X to check if the last ERRORLEVEL was GEQ(greater or equal than) X.
@@ -24,11 +24,8 @@ IF NOT ERRORLEVEL 1 (
   REM Bail early if the x64 MSVC start script is invoked on a 32-bit host OS.
   REM Note: We explicitly aren't supporting x86->x64 cross-compiles.
   IF "%MOZ_MSVCBITS%" == "64" (
-    ECHO.
-    ECHO The MSVC 64-bit toolchain is not supported on a 32-bit host OS. Exiting.
-    ECHO.
-    PAUSE
-    EXIT /B 1
+    SET ERROR=The MSVC 64-bit toolchain is not supported on a 32-bit host OS. Exiting.
+    GOTO _QUIT
   )
   SET WIN64=0
 )
@@ -54,11 +51,8 @@ IF DEFINED MOZ_MSVCVERSION (
     REM Find the MSVC installation directory and bail if none is found.
     REG QUERY !MSVCKEY! /v ProductDir >nul 2>nul
     IF ERRORLEVEL 1 (
-      ECHO.
-      ECHO Microsoft Visual C++ %MOZ_MSVCYEAR% was not found. Exiting.
-      ECHO.
-      PAUSE
-      EXIT /B 1
+      SET ERROR=Microsoft Visual C++ %MOZ_MSVCYEAR% was not found. Exiting.
+      GOTO _QUIT
     )
     FOR /F "tokens=2*" %%A IN ('REG QUERY !MSVCKEY! /v ProductDir') DO SET VCDIR=%%B
   )
@@ -86,11 +80,8 @@ IF DEFINED MOZ_MSVCVERSION (
 
       REM Bail if no Windows SDK is found.
       IF NOT DEFINED SDKDIR (
-        ECHO.
-        ECHO No Windows SDK found. Exiting.
-        ECHO.
-        PAUSE
-        EXIT /B 1
+        SET ERROR=No Windows SDK found. Exiting.
+        GOTO _QUIT
       )
 
       SET SDKVER=8
@@ -98,37 +89,44 @@ IF DEFINED MOZ_MSVCVERSION (
     )
   )
 
-  ECHO Visual C++ %MOZ_MSVCYEAR% Directory: !VCDIR!
-  ECHO Windows SDK Directory: !SDKDIR!
-
   REM Prepend MSVC paths.
   IF "%WIN64%" == "1" (
     IF "%MOZ_MSVCBITS%" == "32" (
       REM Prefer cross-compiling 32-bit builds using the 64-bit toolchain if able to do so.
       IF EXIST "!VCDIR!\bin\amd64_x86\vcvarsamd64_x86.bat" (
         CALL "!VCDIR!\bin\amd64_x86\vcvarsamd64_x86.bat"
-        ECHO Using the MSVC %MOZ_MSVCYEAR% 64-bit cross-compile toolchain.
+        SET TOOLCHAIN=64-bit cross-compile
       )
     ) ELSE IF "%MOZ_MSVCBITS%" == "64" (
       IF EXIST "!VCDIR!\bin\amd64\vcvars64.bat" (
         CALL "!VCDIR!\bin\amd64\vcvars64.bat"
-        ECHO Using the MSVC %MOZ_MSVCYEAR% 64-bit toolchain.
+        SET TOOLCHAIN=64-bit
       )
     )
   ) ELSE IF EXIST "!VCDIR!\bin\vcvars32.bat" (
     CALL "!VCDIR!\bin\vcvars32.bat"
-    ECHO Using the MSVC %MOZ_MSVCYEAR% 32-bit toolchain.
+    SET TOOLCHAIN=32-bit
   )
 
   REM LIB will be defined if vcvars has run. Bail if it isn't.
   IF NOT DEFINED LIB (
-    ECHO.
-    ECHO Unable to call a suitable vcvars script. Exiting.
-    ECHO.
-    PAUSE
-    EXIT /B 1
+    SET ERROR=Unable to call a suitable vcvars script. Exiting.
+    GOTO _QUIT
   )
 )
 
 cd "%USERPROFILE%"
 %MOZILLABUILD%\msys\bin\bash --login -i
+EXIT
+
+
+:_QUIT
+ECHO MozillaBuild Install Directory: %MOZILLABUILD%
+IF DEFINED VCDIR (ECHO Visual C++ %MOZ_MSVCYEAR% Directory: !VCDIR!)
+IF DEFINED SDKDIR (ECHO Windows SDK Directory: !SDKDIR!)
+IF DEFINED TOOLCHAIN (ECHO Trying to use the MSVC %MOZ_MSVCYEAR% !TOOLCHAIN! toolchain.)
+ECHO.
+ECHO %ERROR%
+ECHO.
+PAUSE
+EXIT
