c3f4cf46046974878a66044f08b6fac6cf508acc
Ryan VanderMeulen <ryanvm@gmail.com>
2015-04-13 21:58:28 -0400

    Bug 1143061 - Allow the start-msvc* scripts to be called from a different location.

--- start-shell-msvc2013-x64.bat
+++ start-shell-msvc2013-x64.bat
@@ -4,4 +4,8 @@ SET MOZ_MSVCBITS=64
 SET MOZ_MSVCVERSION=12
 SET MOZ_MSVCYEAR=2013
 
+REM Switch CWD to the current location so that the call to start.shell-bat
+REM doesn't fail if invoked from a different location.
+pushd "%~dp0"
+
 CALL start-shell.bat
