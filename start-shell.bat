f6df580bd19e591b49c1c0f48511abc143f98414
Myk Melez <myk@mykzilla.org>
2016-04-05 11:08:02 -0700

    Bug 1260823 - quote value containing delayfully-expanded variable; r=ryanvm

--- start-shell.bat
+++ start-shell.bat
@@ -55,7 +55,7 @@ REM Find the LLVM installation directory
 REG QUERY "!LLVMKEY!" /ve >nul 2>nul
 IF NOT ERRORLEVEL 1 (
   FOR /F "tokens=2*" %%A IN ('REG QUERY "!LLVMKEY!" /ve') DO SET LLVMDIR=%%B
-  SET PATH=%PATH%;!LLVMDIR!\bin
+  SET PATH="%PATH%;!LLVMDIR!\bin"
 )
 
 REM Set up the MSVC environment if called from one of the start-shell-msvc batch files.
