854082dc884d98c098edadf9d243ca3349421628
Ryan VanderMeulen <ryanvm@gmail.com>
2017-05-23 22:33:06 -0400

    Bug 1346643 - Remove make-mozillabuild.bat in favor of packageit.py. r=gps
    This commit begins the process of converting packageit.py into
    the sole source of truth for packaging MozillaBuild. It removes
    make-mozillabuild.bat in favor of setting the variables directly
    in packageit.py.
    
    It also updates the packaging documentation and does a few other
    minor cleanups. Also, hgwatchman has been temporarily commented
    out due to issues getting it to install.
    
    Things don't quite work right after this patch, but subsequent
    patches will clean up the remaining issues.

--- packageit.py
+++ packageit.py
@@ -1,70 +1,36 @@
-# Creates a mozilla-build installer.
+#!python.exe
+
+# Create a MozillaBuild installer.
 #
-# This script will taint your registry, cause hives, and otherwise screw up
-# the system it's run on. Please do *not* run it on any machine you care about
-# (a temporary VM would be perfect!)
+# This packaging script is intended to be entirely self-contained. However, it's within the realm
+# of possibility of making changes to the host machine it's running on, so it's recommmended to
+# be run within a VM instead.
 #
-# When clicking through installer dialogs, don't run any post-install steps.
-# You won't need to change any paths.
+# System Requirements:
+#   * 64-bit host OS
+#   * Windows 7+
+#   * Existing MozillaBuild installation
 #
-# This script is python instead of shell because running the MSYS installer
-# requires that no MSYS shells be currently running.
+# Usage Instructions:
+#   The script has built-in defaults that should allow for the package to be built simply by
+#   invoking ./packageit.py from a MozillaBuild terminal. It also supports command line arguments
+#   for changing the default paths if desired.
 #
-# = How to Build MozillaBuild =
-# References:
-# http://www.mingw.org/MinGWiki/index.php/MSYSBuildEnvironment
-# http://www.mingw.org/MinGWiki/index.php/Build%20MSYS?PHPSESSID=ec53e47bb122b5dbc18063cb441983be
-# http://mxr.mozilla.org/mozilla/source/tools/build-environment/win32/packageit.py
-# == MSYS Packages ==
-# === Installation ===
-# Install these two packages to the same location:
-# * [http://downloads.sourceforge.net/mingw/MSYS-1.0.10.exe?modtime=1079444447&big_mirror=1 MSYS Base System 1.0.10]
-# * [http://downloads.sourceforge.net/mingw/msysDTK-1.0.1.exe?modtime=1041430674&big_mirror=1 MSYS Developer Toolkit 1.0.1]
-# 
-# Open up an MSYS shell and do the following:
-#  mkdir /msys
-#  cd /msys
-#  tar -zvxf msysDVLPR-1.0.0-alpha-1.tar.gz
-#  cd /msys/lib/gcc-lib/i686-pc-msys/2.95.3-1
-#  mv specs specs.orig
-#  sed 's|/usr|/msys|g' < specs.orig > specs
-# 
-# Download and unzip the [http://downloads.sourceforge.net/mingw/w32api-3.10.tar.gz?modtime=1186139469&big_mirror=1 w32api] package to somewhere outside of your MSYS install. Open up an MSYS shell and copy include/wincon.h to /msys/include.
-# 
-# * Open an MSYS shell and do the following
-#  cd /msys/lib/gcc-lib/i686-pc-msys/2.95.3-1/include/msys
-# Open the file up in vim and go to line 48. It should read:
-#  #define CTRL(c'h')          ((ch)&0x1F)
-# Change it to read:
-#  #define CTRL(ch)          ((ch)&0x1F)
-# 
-# === Path Setup ===
-# Open up an MSYS shell, edit ~/.profile and add the following line:
-#  export PATH=".:/msys/bin:/usr/local/bin:/bin:$PATH"
-# 
-# == Other Applications ==
-# * [http://www.bastet.com/uddu.zip unix2dos]
-# ** Put the exe files in c:\windows
-# * [http://www.python.org/ftp/python/2.7.5/python-2.7.5.msi Python 2.7]
-# * [http://code.google.com/p/unsis/downloads/detail?name=nsis-2.46-Unicode-setup.exe NSIS]
-# * [http://mxr.mozilla.org/mozilla/source/tools/build-environment/win32/unz552xN.exe?raw=1&ctype=application/octet-stream unzip]
-# ** Extract to c:\program files\unzip
-# * Append ';c:\python25;c:\program files\nsis;c:\program files\unzip' to path.
-# 
+
 from subprocess import check_call
 from os import getcwd, remove, environ, chdir, walk, rename, remove
 from os.path import dirname, join, split, abspath, exists
 from shutil import rmtree, copyfile, copytree
 import distutils.core, optparse, tarfile, urllib2
 
+# Set default values for the source and stage directories.
 sourcedir = join(split(abspath(__file__))[0])
-stagedir = getcwd()
-msysdir = "c:\\msys\\1.0"
+stagedir = "c:\\mozillabuild-stage"
 
+# Override the source and/or stage directory locations if otherwise specified.
 oparser = optparse.OptionParser()
 oparser.add_option("-s", "--source", dest="sourcedir")
 oparser.add_option("-o", "--output", dest="stagedir")
-oparser.add_option("-m", "--msys", dest="msysdir")
 (options, args) = oparser.parse_args()
 
 if len(args) != 0:
@@ -74,17 +40,19 @@ if options.sourcedir:
     sourcedir = options.sourcedir
 if options.stagedir:
     stagedir = options.stagedir
-if options.msysdir:
-    msysdir = options.msysdir
 
 environ["MOZ_STAGEDIR"] = stagedir
 environ["MOZ_SRCDIR"] = sourcedir
 
-print("Source file location: " + sourcedir)
+print("Source location: " + sourcedir)
 print("Output location: " + stagedir)
 
-if exists(join(stagedir, "mozilla-build")):
-    check_call(["cmd.exe", "/C", "rmdir /S /Q %s" % join(stagedir, "mozilla-build")])
+# Remove the old stage directory if it's already present.
+# We use cmd.exe instead of sh.rmtree because it's more forgiving of open handles than
+# Python is (i.e. not hard-stopping if you happen to have the stage directory open in
+# Windows Explorer while testing.
+if exists(stagedir):
+    check_call(["cmd.exe", "/C", "rmdir /S /Q %s" % stagedir])
 
 # Install 7-Zip. Create an administrative install point and copy the files to stage rather
 # than using a silent install to avoid installing the shell extension on the host machine.
@@ -114,17 +82,15 @@ check_call([join(stagedir, "mozilla-build", "python", "python.exe"),
 # Update setuptools to the latest version
 check_call([join(stagedir, "mozilla-build", "python", "python.exe"),
             "-m", "pip", "install", "--upgrade", "setuptools"])
-# Install hgwatchman
-check_call([join(stagedir, "mozilla-build", "python", "python.exe"),
-            "-m", "pip", "install", "hgwatchman"])
+# Install hgwatchman - TEMPORARILY COMMENTED OUT DUE TO BUSTAGE
+#check_call([join(stagedir, "mozilla-build", "python", "python.exe"),
+#            "-m", "pip", "install", "hgwatchman"])
 # Install virtualenv
 check_call([join(stagedir, "mozilla-build", "python", "python.exe"),
             "-m", "pip", "install", "virtualenv"])
 # Install Mercurial
 check_call([join(stagedir, "mozilla-build", "python", "python.exe"),
             "-m", "pip", "install", "mercurial"])
-# Patch the downloaded Mercurial source prior to packaging (if needed)
-#check_call([join(msysdir, "bin", "patch.exe"), "-p1", "-i", join(sourcedir, "patch-name.here")])
 
 # Find any occurrences of hardcoded interpreter paths in the Scripts directory and change them
 # to a generic python.exe instead. Awful, but distutils hardcodes the interpreter path in the
@@ -152,8 +118,7 @@ check_call([join(sourcedir, "KDiff3-32bit-Setup_0.9.98.exe"),
 # Run an MSYS shell to perform the following tasks:
 # * Extract MSYS packages and rebase DLLs
 # * Install other packages (UPX, info-zip, etc)
-check_call([join(msysdir, "bin", "sh.exe"), "--login",
-            join(sourcedir, "packageit.sh")])
+check_call(["sh.exe", join(sourcedir, "packageit.sh")])
 
 # Embed some manifests to make Vista happy
 def embedmanifest(f, mf):
