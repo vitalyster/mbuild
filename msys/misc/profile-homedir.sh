513582af0204d6731840bb6028d2edbd7721426e
Ryan VanderMeulen <ryanvm@gmail.com>
2017-06-06 16:46:59 -0400

    Bug 1262249 - Remove the unnecessary call to .bash_profile in profile-homedir.sh.
    .bash_profile is called automatically bash when invoked via the
    --login flag, which is how start-shell.bat launches it.

--- msys/misc/profile-homedir.sh
+++ msys/misc/profile-homedir.sh
@@ -4,7 +4,3 @@ if test -n "$USERPROFILE" -a "$HOME" = "/home/$LOGNAME"; then
     HOME=$(cd "$USERPROFILE" && pwd)
 fi
 export HOME
-
-if test -f "$HOME/.bash_profile"; then
-    . "$HOME/.bash_profile"
-fi
